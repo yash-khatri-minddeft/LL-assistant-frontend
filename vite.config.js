fde7268f3e2e0b36eda640c3d357e89931ade1f1
yash-khatri-minddeft <yash.khatri@minddeft.net>
2023-11-22 16:27:42 +0530

    config change

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,14 @@
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd());
   return {
     plugins: [react()],
     server: {
       proxy: {
         "/api": {
-          target: env.VITE_BASE_URL,
+          target: "https://api-ll-assistant.onrender.com",
           changeOrigin: true,
           rewrite: (path) => path.replace(/^\/api/, ""),
         },
